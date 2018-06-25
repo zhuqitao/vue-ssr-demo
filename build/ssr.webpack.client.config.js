7a1e68fa351ddcf2831ae022c57e8d40240297a4
zhuqitao <1362335135@qq.com>
2018-06-25 09:20:37 +0800

    使用vue2.x 初始化项目

---
+++ build/ssr.webpack.client.config.js
@@ -0,0 +1,1 @@
+ssr.webpack.client.config.js
---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="app">
+        <h1>vue-ssr</h1>
+        <router-link class="link" to="/comp1">to comp1</router-link>
+        <router-link class="link" to="/comp2">to comp2</router-link>
+
+        <router-view class="view"></router-view>
+    </div>
+</template>
+<script>
+export default {
+        data () {
+            return {
+                msg: ''
+            }
+        }
+    }
+</script>
