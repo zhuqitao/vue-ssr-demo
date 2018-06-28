0b7879a2e6fd45d12ab13032ead3304d65f88905
zhuqitao <1362335135@qq.com>
2018-06-28 11:17:08 +0800

    服务端应用

--- build/ssr.webpack.base.config.js
+++ build/ssr.webpack.base.config.js
@@ -19,12 +19,12 @@ module.exports = {
     module: {
         noParse: /es6-promise\.js$/,
         rules: [
-            {
-                test: /\.(js|vue)/,
-                use: 'eslint-loader',
-                enforce: 'pre',
-                exclude: /node_modules/
-            },
+            // {
+            //     test: /\.(js|vue)/,
+            //     use: 'eslint-loader',
+            //     enforce: 'pre',
+            //     exclude: /node_modules/
+            // },
             {
                 test: /\.vue$/,
                 use: {
